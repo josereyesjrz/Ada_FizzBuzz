eb2f1e938ba82a8746f1917c3be538c7a7b8fa78
jreyes <josereyes5396@outlook.com>
2016-11-18 17:50:19 -0400

    Adds finished FizzBuzz

---
+++ ada_fizzbuzz.adb
@@ -0,0 +1,23 @@
+-- FizzBuzz in Ada
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use  Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure Ada_FizzBuzz is
+	N : Integer := 1;
+begin
+	FB_Loop :
+		loop
+			if N mod 15 = 0 then
+				Put_Line("FizzBuzz");
+			elsif N mod 5 = 0 then
+				Put_Line("Buzz");
+			elsif N mod 3 = 0 then
+				Put_Line("Fizz");
+			else
+				Put(N);
+				New_Line;
+			end if;
+			N := N + 1;
+		exit FB_Loop when N > 100;
+		end loop FB_Loop;
+end Ada_FizzBuzz;
