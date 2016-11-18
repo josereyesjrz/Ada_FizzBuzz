ed58085f4aace2b946c3e285b859fe493a643128
jreyes <josereyes5396@outlook.com>
2016-11-18 18:24:35 -0400

    Changes loop implementation to a for loop

--- ada_fizzbuzz.adb
+++ ada_fizzbuzz.adb
@@ -3,9 +3,9 @@ with Ada.Text_IO, Ada.Integer_Text_IO;
 use  Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure Ada_FizzBuzz is
-  N : Integer := 1;
 begin
   FB_Loop :
+    for N in 1 .. 100
     loop
       if N mod 15 = 0 then
         Put_Line("FizzBuzz");
@@ -17,7 +17,5 @@ begin
         Put(N, 1); -- Print non FizzBuzz integer with width of 1 space
         New_Line;
       end if;
-      N := N + 1;
-    exit FB_Loop when N > 100;
     end loop FB_Loop;
 end Ada_FizzBuzz;
